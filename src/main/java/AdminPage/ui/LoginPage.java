3a3d5a3b5b287d20ef5ef17390f9f8521d8055ed
jabels1121 <jabels1121@gmail.com>
2018-02-19 23:46:02 +0300

    createProject

---
+++ src/main/java/AdminPage/ui/LoginPage.java
@@ -0,0 +1,12 @@
+package AdminPage.ui;
+
+import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.By;
+
+public class LoginPage extends PageObject{
+
+
+    public static By LOGIN_FIELD;
+    public static By PASSWORD_FIELD;
+
+}
